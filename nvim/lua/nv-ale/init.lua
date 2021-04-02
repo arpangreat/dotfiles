c81e81b069361e083715e87b8d0f6bf4ed0f5895
arpangreat <arpanthegreat41@gmail.com>
2021-04-02 22:00:59 +0530

    some screenshot keybindings for sway and galaxyline

--- nvim/lua/nv-ale/init.lua
+++ nvim/lua/nv-ale/init.lua
@@ -2,8 +2,8 @@
 vim.cmd("let g:ale_sign_error = '§'")
 vim.cmd("let g:ale_sign_warning = 'Þ'")
 
-vim.cmd('let g:airline#extensions#ale#enabled = 1')
-vim.cmd('set statusline=%{LinterStatus()}')
+-- vim.cmd('let g:airline#extensions#ale#enabled = 1')
+-- vim.cmd('set statusline=%{LinterStatus()}')
 vim.cmd('nmap <silent> <C-l> <Plug>(ale_previous_wrap)')
 vim.cmd('nmap <silent> <C-h> <Plug>(ale_next_wrap)')
 
