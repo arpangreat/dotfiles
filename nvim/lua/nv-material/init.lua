c8512951cbdb64b4d38221a8d05f80cfbbe6fa73
arpangreat <arpanthegreat41@gmail.com>
2021-05-17 01:06:56 +0530

    neofetch config

--- nvim/lua/nv-material/init.lua
+++ nvim/lua/nv-material/init.lua
@@ -2,7 +2,11 @@ vim.g.material_flat_ui = 1
 vim.g.material_italic_comments = 1
 vim.g.material_italic_keywords = 1
 vim.g.material_italic_functions = 1
+vim.g.material_italic_variables = 1
 vim.g.material_style = 'palenight'
+-- vim.g.material_contrast = false
+vim.g.material_borders = true
+-- vim.g.material_disable_background = 0
 
 require('colorbuddy').colorscheme('material')
 vim.cmd("colorscheme material")
