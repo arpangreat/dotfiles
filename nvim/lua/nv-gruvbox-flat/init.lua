dc6b3ddcb0404648f5b8deae4e81b7ebf592d025
arpangreat <arpanthegreat41@gmail.com>
2021-05-26 03:52:35 +0530

    new colorscheme for nvim

---
+++ nvim/lua/nv-gruvbox-flat/init.lua
@@ -0,0 +1,7 @@
+vim.api.nvim_exec([[
+colorscheme gruvbox-flat
+highlight Normal guibg=none
+]], true)
+vim.g.gruvbox_flat_style = "dark"
+vim.g.gruvbox_italic_functions = true
+vim.g.gruvbox_italic_variables = true
