8cc3ae247ea90ef139e5c46a5d551baef21014f1
arpangreat <arpanthegreat41@gmail.com>
2021-05-28 00:45:43 +0530

    some telescope stuff

--- nvim/lua/arpangreat/telescope.lua
+++ nvim/lua/arpangreat/telescope.lua
@@ -3,7 +3,7 @@ local actions = require('telescope.actions')
 require('telescope').setup {
     defaults = {
         file_sorter = require('telescope.sorters').get_fzy_sorter,
-        prompt_prefix = ' >',
+        prompt_prefix = '$> ',
         color_devicons = true,
 
         file_previewer   = require('telescope.previewers').vim_buffer_cat.new,
