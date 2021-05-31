aae834fbe6da06c85233e83c2d2614aca731863b
arpangreat <arpanthegreat41@gmail.com>
2021-05-31 12:33:33 +0530

    Nebulous is the best colorscheme for nvim

--- nvim/lua/arpangreat/telescope.lua
+++ nvim/lua/arpangreat/telescope.lua
@@ -40,12 +40,13 @@ require('telescope').setup {
         --     override_generic_sorter = false,
         --     override_file_sorter = true,
         -- },
-		 fzf = {
-      override_generic_sorter = false, -- override the generic sorter
-      override_file_sorter = true,     -- override the file sorter
-      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
-                                       -- the default case_mode is "smart_case"
-    },
+	fzf = {
+		fuzzy = true,
+		override_generic_sorter = false, -- override the generic sorter
+		override_file_sorter = true,     -- override the file sorter
+		case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+					-- the default case_mode is "smart_case"
+	 },
 	 frecency = {
       show_scores = false,
       show_unindexed = true,
--- nvim/lua/nv-nightbuddy/init.lua
+++
@@ -1,8 +0,0 @@
-vim.g.nb_style = "twilight"
-require('colorbuddy').colorscheme('nightbuddy')
-vim.api.nvim_exec([[
-highlight Normal guibg=none
-highlight Comment cterm=italic gui=italic
-highlight Keyword cterm=italic gui=italic
-highlight Function cterm=italic gui=italic
-]], true)
