610161735bf729b6ceb95b2dde08c5c21baa67a3
arpangreat <arpanthegreat41@gmail.com>
2021-06-06 02:54:29 +0530

    some clean ups and patches for dwm

--- dwm/config.h
+++ dwm/config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx     = 6;
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Operator Mono Lig Book:size=10" };
@@ -19,7 +20,7 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1: Terminal 🗔", "2: Files 📂", "3: Browser 🌐", "4", "5", "6", "7", "8", "9: MultiMedia 🎬" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -29,6 +30,10 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Alacritty",NULL,       NULL,       0,            0,           -1 },
+	{ "dolphin",  NULL,       NULL,       0 << 3,       0,           -1 },
+	{ "Brave",    NULL,       NULL,       0 << 4,       0,           -1 },
+	{ "vlc",      NULL,       NULL,       8,            0,           -1 },
 };
 
 /* layout(s) */
