9234f93a3f38a329ce67d84ca0855090baaec4d6
arpangreat <arpanthegreat41@gmail.com>
2021-06-10 03:06:23 +0530

    some changes in dwm configs , smartgaps

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -8,7 +8,7 @@ static const unsigned int gappih    = 60;       /* horiz inner gap between windo
 static const unsigned int gappiv    = 60;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 60;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 60;       /* vert outer gap between windows and screen edge */
-static       int smartgaps          = 6;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Operator Mono Lig Book:size=10" };
