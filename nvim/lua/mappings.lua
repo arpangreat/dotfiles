401bb908375b36c1bb25d36ffc9e759da3a2455b
arpangreat <arpanthegreat41@gmail.com>
2021-08-23 22:55:44 +0530

    Treesitter-unit-plugin implementation, ( made by me )

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -278,5 +278,11 @@ vim.api.nvim_set_keymap('i', '<C-k>', '<esc>:m .-2<CR>==', { noremap = true, sil
 vim.api.nvim_set_keymap('n', '<LocalLeader>j', ':m .+1<CR>==', { noremap = true, silent = false, expr = false })
 vim.api.nvim_set_keymap('n', '<LocalLeader>k', ':m .-2<CR>==', { noremap = true, silent = false, expr = false })
 
+-- Treesitter Unit
+vim.api.nvim_set_keymap('n', '<Leader>tus', ':lua require("treesitter-unit-plugin").select()<CR>', { noremap = true, silent = false, expr = false })
+vim.api.nvim_set_keymap('n', '<Leader>tud', ':lua require("treesitter-unit-plugin").delete()<CR>', { noremap = true, silent = false, expr = false })
+vim.api.nvim_set_keymap('n', '<Leader>tuc', ':lua require("treesitter-unit-plugin").change()<CR>', { noremap = true, silent = false, expr = false })
+vim.api.nvim_set_keymap('o', '<Leader>tuy', ':<C-u>lua require("treesitter-unit-plugin").select(true)<CR>:y<CR>', { noremap = true, silent = false, expr = false })
+
 -- HACK: Formating Big Lines with g
 vim.api.nvim_set_keymap('n', '<Leader>fg', "<cmd>g/ ./ normal gqq<CR>", { noremap = true, silent = false, expr = false })
