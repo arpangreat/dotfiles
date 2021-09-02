1ff91343a422aed40d1e761023212017be9dbd29
arpangreat <arpanthegreat41@gmail.com>
2021-09-02 22:00:44 +0530

    nvim clean ups

--- nvim/lua/nv-autopairs/init.lua
+++ nvim/lua/nv-autopairs/init.lua
@@ -2,3 +2,7 @@
 vim.cmd('let g:AutoPairsFlyMode = 1')
 vim.cmd("let g:AutoPairsShortcutBackInsert = '<M-b>'")
 
+vim.api.nvim_exec([[
+  au FileType lua      let b:AutoPairs = AutoPairsDefine({'then' : 'end', 'do': 'end'})
+]], true)
+
