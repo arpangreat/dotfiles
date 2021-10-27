773772d6a4fa5d94901e11b61999c81b50613013
arpangreat <arpanthegreat41@gmail.com>
2021-10-27 21:02:11 +0530

    nvim-cmp clean ups

--- nvim/lua/nv-nvim-cmp/init.lua
+++ nvim/lua/nv-nvim-cmp/init.lua
@@ -1,11 +1,3 @@
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local feedkey = function(key, mode)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
@@ -27,17 +19,19 @@ cmp.setup {
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }),
-     ["<Tab>"] = cmp.mapping(function()
-      if vim.fn["vsnip#available"]() == 1 then
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if vim.fn["vsnip#available"](1) == 1 then
         feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      else
+        fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
       end
     end, { "i", "s" }),
 
-    ["<S-Tab>"] = cmp.mapping(function()
+    --[[ ["<S-Tab>"] = cmp.mapping(function()
       if vim.fn["vsnip#jumpable"](-1) == 1 then
         feedkey("<Plug>(vsnip-jump-prev)", "")
       end
-    end, { "i", "s" }),
+    end, { "i", "s" }), ]]
   },
 
   sources = cmp.config.sources({
