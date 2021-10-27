fd066516cd4f757612e0b0578f191ec6746ecae8
arpangreat <arpanthegreat41@gmail.com>
2021-10-27 21:27:03 +0530

    lua clean ups

--- nvim/lua/lua-ls.lua
+++ nvim/lua/lua-ls.lua
@@ -70,27 +70,31 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true
 require'lspconfig'.sumneko_lua.setup {
     cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
     settings = {
-        Lua = {
-            runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                version = 'LuaJIT',
-                -- Setup your lua path
-                path = vim.split(package.path, ';')
-            },
-            diagnostics = {
-                -- Get the language server to recognize the `vim` global
-                globals = {'vim'}
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-               library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                          ["${3rd}/love2d/library"] = true},
-               maxPreload = 2000,
-               checkThirdParty = false,
-            }
-        }
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+        -- Setup your lua path
+        path = vim.split(package.path, ';')
+      },
+      completion = {
+        callSnippet = 'Replace',
+      },
+      diagnostics = {
+        enable = true,
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'}
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = {[vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
+          ["${3rd}/love2d/library"] = true},
+        maxPreload = 2000,
+        checkThirdParty = false,
+      },
     },
+  },
     on_attach = on_attach,
     capabilities = capabilities,
 }
