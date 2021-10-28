654bc0ab439e9d34173d359cef47b65d2b00b045
arpangreat <arpanthegreat41@gmail.com>
2021-10-28 20:27:18 +0530

    clean ups

---
+++ nvim/lua/nv-nightfox/init.lua
@@ -0,0 +1,31 @@
+local nightfox = require('nightfox')
+
+-- This function set the configuration of nightfox. If a value is not passed in the setup function
+-- it will be taken from the default configuration above
+nightfox.setup({
+  fox = "duskfox", -- change the colorscheme to use nordfox
+  transparent = true,
+  styles = {
+    comments = "italic", -- change style of comments to be italic
+    keywords = "italic,bold", -- change style of keywords to be bold
+    functions = "italic,bold", -- styles can be a comma separated list
+    strings = "italic",
+    variable = "bold",
+  },
+  inverse = {
+    match_paren = true, -- inverse the highlighting of match_parens
+    visual = true,
+    search = true,
+  },
+  colors = {
+    red = "#FF000", -- Override the red color for MAX POWER
+    bg_alt = "#000000",
+  },
+  hlgroups = {
+    TSPunctDelimiter = { fg = "${red}" }, -- Override a highlight group with the color red
+    LspCodeLens = { bg = "#000000", style = "italic" },
+  }
+})
+
+-- Load the configuration set above and apply the colorscheme
+nightfox.load()
--- nvim/lua/nv-nvim-cmp/init.lua
+++ nvim/lua/nv-nvim-cmp/init.lua
@@ -3,6 +3,7 @@ local feedkey = function(key, mode)
 end
 
 local lspkind = require('lspkind')
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require'cmp'
 cmp.setup {
   snippet = {
@@ -73,13 +74,4 @@ cmp.setup {
 }
 
 -- you need setup cmp first put this after cmp.setup()
-require("nvim-autopairs.completion.cmp").setup({
-  map_cr = true, --  map <CR> on insert mode
-  map_complete = true, -- it will auto insert `(` (map_char) after select function or method item
-  auto_select = true, -- automatically select the first item
-  insert = false, -- use insert confirm behavior instead of replace
-  map_char = { -- modifies the function or method delimiter by filetypes
-    all = '(',
-    tex = '{'
-  }
-})
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done() )
