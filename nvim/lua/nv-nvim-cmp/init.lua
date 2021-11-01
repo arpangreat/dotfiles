3cf9d22739575fef38cfe4011484ff091e5879bf
arpangreat <arpanthegreat41@gmail.com>
2021-11-02 00:53:19 +0530

    @arpangreat cmp_git update

--- nvim/lua/nv-nvim-cmp/init.lua
+++ nvim/lua/nv-nvim-cmp/init.lua
@@ -91,5 +91,43 @@ cmp.setup {
   },
 }
 
+require("cmp_git").setup({
+    -- defaults
+    filetypes = { "gitcommit" },
+    remotes = { "upstream", "origin" }, -- in order of most to least prioritized
+    git = {
+        commits = {
+            limit = 100,
+        },
+    },
+    github = {
+        issues = {
+            filter = "all", -- assigned, created, mentioned, subscribed, all, repos
+            limit = 100,
+            state = "open", -- open, closed, all
+        },
+        mentions = {
+            limit = 100,
+        },
+        pull_requests = {
+            limit = 100,
+            state = "open", -- open, closed, merged, all
+        },
+    },
+    gitlab = {
+        issues = {
+            limit = 100,
+            state = "opened", -- opened, closed, all
+        },
+        mentions = {
+            limit = 100,
+        },
+        merge_requests = {
+            limit = 100,
+            state = "opened", -- opened, closed, locked, merged
+        },
+    },
+})
+
 -- you need setup cmp first put this after cmp.setup()
 cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done() )
