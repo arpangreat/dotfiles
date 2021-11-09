f109b4411a241c1203059920a3bf02e0d8615152
arpangreat <arpanthegreat41@gmail.com>
2021-11-10 01:32:26 +0530

    Comment.nvim stuff

--- nvim/lua/nv-comment-nvim/init.lua
+++ nvim/lua/nv-comment-nvim/init.lua
@@ -49,22 +49,8 @@ require("Comment").setup({
 
 	---Pre-hook, called before commenting the line
 	---@type function
-	pre_hook = function(ctx)
-		local U = require("Comment.utils")
-
-		local location = nil
-		if ctx.ctype == U.ctype.block then
-			location = require("ts_context_commentstring.utils").get_cursor_location()
-		elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-			location = require("ts_context_commentstring.utils").get_visual_start_location()
-		end
-
-		return require("ts_context_commentstring.internal").calculate_commentstring({
-			key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-			location = location,
-		})
-	end,
-
+	pre_hook = nil,
+	-- Only calculate commentstring for tsx filetypes
 	---Post-hook, called after commenting is done
 	---@type function
 	post_hook = nil,
