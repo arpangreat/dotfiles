d473abc9dd511095a65d7329d4c4354fa67024df
arpangreat <arpanthegreat41@gmail.com>
2021-11-10 00:55:26 +0530

    clean ups

--- nvim/lua/custom-Telescope.lua
+++ nvim/lua/custom-Telescope.lua
@@ -6,29 +6,51 @@ local sorters = require("telescope.sorters")
 local themes = require("telescope.themes")
 
 local mini = {
-  layout_strategy = "vertical",
-  layout_config = {
-    height = 10,
-    width = 0.3,
-  },
+	layout_strategy = "vertical",
+	layout_config = {
+		height = 10,
+		width = 0.3,
+	},
 }
 
 function enter(prompt_bufnr)
-  local selected = actions_state.get_selected_entry()
-  -- print(vim.inspect(selected))
-  local cmd = "colorscheme " .. selected[1]
-  vim.cmd(cmd)
-  actions.close(prompt_bufnr)
+	local selected = actions_state.get_selected_entry()
+	-- print(vim.inspect(selected))
+	local cmd = "colorscheme " .. selected[1]
+	vim.cmd(cmd)
+	actions.close(prompt_bufnr)
+end
+
+function next_color(prompt_bufnr)
+	actions.move_selection_next(prompt_bufnr)
+	local selected = actions_state.get_selected_entry()
+	-- print(vim.inspect(selected))
+	local cmd = "colorscheme " .. selected[1]
+	vim.cmd(cmd)
+end
+
+function prev_color(prompt_bufnr)
+	actions.move_selection_previous(prompt_bufnr)
+	local selected = actions_state.get_selected_entry()
+	-- print(vim.inspect(selected))
+	local cmd = "colorscheme " .. selected[1]
+	vim.cmd(cmd)
 end
 
 local opts = {
-  finder = finders.new_table {"tokyonight", "nightfly", "katppuccino"},
-  sorter = sorters.get_generic_fuzzy_sorter({}),
+	finder = finders.new_table({ "tokyonight", "nightfly", "katppuccino" }),
+	sorter = sorters.get_generic_fuzzy_sorter({}),
 
-  attach_mappings = function (prompt_bufnr, map)
-    map("i", "<CR>", enter)
-    return true
-  end,
+	attach_mappings = function(prompt_bufnr, map)
+		map("i", "<CR>", enter)
+		map("i", "<C-j>", next_color)
+		map("i", "<C-k>", prev_color)
+
+		map("n", "<CR>", enter)
+		map("n", "j", next_color)
+		map("n", "k", prev_color)
+		return true
+	end,
 }
 
 local dropdown = themes.get_dropdown()
