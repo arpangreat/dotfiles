91c9abb66dea9f31520b897766e08cd232de347f
arpangreat <arpanthegreat41@gmail.com>
2021-12-19 03:32:29 +0530

    wezterm

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require("wezterm")
+return {
+	font = wezterm.font("FiraCodeNerdFontComplete-Regular"),
+	color_scheme = "wezterm_tokyonight_storm.toml",
+	color_scheme_dirs = { "/home/arpangreat/dotfiles/wezterm" },
+	window_background_opacity = 0.6,
+}
