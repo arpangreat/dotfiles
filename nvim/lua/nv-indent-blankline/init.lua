85f5757aed080b4f42ffaca9847eebac4bc6f028
arpangreat <arpanthegreat41@gmail.com>
2022-01-07 19:11:24 +0530

    clean ups

--- nvim/lua/nv-indent-blankline/init.lua
+++ nvim/lua/nv-indent-blankline/init.lua
@@ -1,6 +1,6 @@
 vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
-vim.opt.listchars:append("eol:↴")
+-- vim.opt.listchars:append("eol:↴")
 vim.g.indent_blankline_filetype_exclude = "dashboard"
 vim.g.indent_blankline_use_treesitter = true
 
