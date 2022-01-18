c35ddc8b3097653a454fd187318fcfe1d21e7a62
arpangreat <arpanthegreat41@gmail.com>
2022-01-18 19:55:26 +0530

    clean ups

--- nvim/lua/nv-nvim-cmp/init.lua
+++ nvim/lua/nv-nvim-cmp/init.lua
@@ -54,7 +54,7 @@ cmp.setup({
 		{ name = "buffer" },
 		{ name = "treesitter" },
 		{ name = "nvim_lua" },
-		{ name = "cmp_git" },
+		{ name = "path" },
 		-- { name = 'vsnip' }
 	}),
 
@@ -93,44 +93,6 @@ cmp.setup({
 	experimental = {
 		native_menu = false,
 		ghost_text = true,
-	},
-})
-
-require("cmp_git").setup({
-	-- defaults
-	filetypes = { "gitcommit" },
-	remotes = { "upstream", "origin" }, -- in order of most to least prioritized
-	git = {
-		commits = {
-			limit = 100,
-		},
-	},
-	github = {
-		issues = {
-			filter = "all", -- assigned, created, mentioned, subscribed, all, repos
-			limit = 100,
-			state = "open", -- open, closed, all
-		},
-		mentions = {
-			limit = 100,
-		},
-		pull_requests = {
-			limit = 100,
-			state = "open", -- open, closed, merged, all
-		},
-	},
-	gitlab = {
-		issues = {
-			limit = 100,
-			state = "opened", -- opened, closed, all
-		},
-		mentions = {
-			limit = 100,
-		},
-		merge_requests = {
-			limit = 100,
-			state = "opened", -- opened, closed, locked, merged
-		},
 	},
 })
 
