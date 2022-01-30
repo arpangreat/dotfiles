a27a9994bc463db47d134483ce5549ce906100a0
arpangreat <arpanthegreat41@gmail.com>
2022-01-31 02:31:10 +0530

    clean ups

--- custom-scripts/tmux-cht.sh
+++ custom-scripts/tmux-cht.sh
@@ -8,7 +8,7 @@ read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/dotfiles/Tmux/.tmux-cht-languages; then
     query=`echo $query | tr ' ' '+'`
-    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+    bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done;"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+    bash -c "curl -s cht.sh/$selected~$query | less"
 fi
