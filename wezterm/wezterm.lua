184e0d5f7f821da84b980c96c6d05548c276d5e7
arpangreat <arpanthegreat41@gmail.com>
2022-02-20 21:47:26 +0530

    clean ups

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,13 @@
 local wezterm = require("wezterm")
 return {
-	font = wezterm.font("FiraCodeNerdFontComplete-Regular"),
-	color_scheme = "wezterm_tokyonight_storm.toml",
+	font = wezterm.font("Fira Code Retina", { weight = "Regular", italic = false }),
+	font = wezterm.font("Fira Code", { weight = "Regular", italic = true }),
+	font = wezterm.font("Fira Code", { weight = "Bold", italic = false }),
+	font_size = 15.0,
+	color_scheme = "Catppuccin",
 	color_scheme_dirs = { "/home/arpangreat/dotfiles/wezterm" },
+	default_cursor_style = "SteadyBar",
+	-- cursor_blink_rate = 500,
+	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.6,
 }
