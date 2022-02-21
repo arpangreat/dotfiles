27c317bba58b92b3b345ed69af88d97794104b8b
arpangreat <arpanthegreat41@gmail.com>
2022-02-21 22:09:00 +0530

    clean ups

--- nvim/lua/nv-gitsigns/init.lua
+++ nvim/lua/nv-gitsigns/init.lua
@@ -1,23 +1,37 @@
-require('gitsigns').setup {
-  signs = {
-    -- TODO add hl to colorscheme
-    add          = {hl = 'GitSignsAdd'   , text = '▎', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '契', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '▎', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-  numhl = false,
-  linehl = false,
-  keymaps = {
-    -- Default keymap options
-    noremap = true,
-    buffer = true,
-  },
-  watch_gitdir = {
-    interval = 1000
-  },
-  sign_priority = 6,
-  update_debounce = 200,
-  status_formatter = nil, -- Use default
-}
+require("gitsigns").setup({
+	signs = {
+		-- TODO add hl to colorscheme
+		add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+		change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+		delete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		topdelete = { hl = "GitSignsDelete", text = "契", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+		changedelete = {
+			hl = "GitSignsChange",
+			text = "▎",
+			numhl = "GitSignsChangeNr",
+			linehl = "GitSignsChangeLn",
+		},
+	},
+	numhl = false,
+	linehl = false,
+	word_diff = true,
+	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+	current_line_blame_opts = {
+		virt_text = true,
+		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+		delay = 500,
+		ignore_whitespace = false,
+	},
+	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+	keymaps = {
+		-- Default keymap options
+		noremap = true,
+		buffer = true,
+	},
+	watch_gitdir = {
+		interval = 1000,
+	},
+	sign_priority = 6,
+	update_debounce = 200,
+	status_formatter = nil, -- Use default
+})
