118ef39900f22e3e7334092e7cae9db178124603
arpangreat <arpanthegreat41@gmail.com>
2022-02-25 23:19:33 +0530

    clean ups

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -29,8 +29,7 @@ run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 # fi
 
 #Some ways to set your wallpaper besides variety or nitrogen
-feh --bg-scale
-/home/arpangreat/dotfiles/wallpaper/devyg41-28cd223e-1a1f-48b6-87c7-41c0064fe163.jpg &
+feh --bg-scale /home/arpangreat/dotfiles/wallpaper/devyg41-28cd223e-1a1f-48b6-87c7-41c0064fe163.jpg &
 run xmodmap /home/arpangreat/dotfiles/i3/swap &
 setxkbmap us
 
