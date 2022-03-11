754666e9ea9daa0f4197349e81622277bfac7cbf
arpangreat <arpanthegreat41@gmail.com>
2022-03-12 01:31:39 +0530

    nvim clean ups

--- nvim/lua/nv-lualine/init.lua
+++ nvim/lua/nv-lualine/init.lua
@@ -1,5 +1,13 @@
+local gps = require("nvim-gps")
+
 require("lualine").setup({
 	options = { theme = require("lualine.themes.palenight") },
 	-- options = { theme = "catppuccin" },
 	-- options = { theme = "tokyonight" },
+	sections = {
+		lualine_c = {
+			{'filename'},
+			{  gps.get_location, cond = gps.is_available },
+		}
+	}
 })
