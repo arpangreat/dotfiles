9b591c8ba0ab857b45d61352a76032fcd461e7b4
arpangreat <arpanthegreat41@gmail.com>
2022-05-17 04:22:01 +0530

    clean ups

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -31,7 +31,8 @@ run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 #Some ways to set your wallpaper besides variety or nitrogen
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/devyg41-28cd223e-1a1f-48b6-87c7-41c0064fe163.jpg &
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/w1.jpg
-feh --bg-scale /home/arpangreat/dotfiles/wallpaper/minimalist11.jpg
+# feh --bg-scale /home/arpangreat/dotfiles/wallpaper/minimalist11.jpg
+feh --bg-center /home/arpangreat/Pictures/Snapchat-612959769.jpg
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/neon.png &
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/woods.jpg
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/wallpaperflare.com_wallpaper.jpg
