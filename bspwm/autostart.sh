36a38e2b2fbda8fc35c4b9d8ebf155f7ead5b145
arpangreat <arpanthegreat41@gmail.com>
2022-08-09 22:32:06 +0530

    clean ups

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -12,7 +12,7 @@ function run {
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-# xrandr --output HDMI1 --mode 1920x1080 --rate 74.97 --primary --left-of eDP1 --mode 1920x1080 --rate 60
+xrandr --output HDMI-1 --mode 1920x1080 --rate 74.97 --primary --left-of eDP-1 --mode 1920x1080 --rate 60
 #autorandr horizontal
 
 $HOME/.config/polybar/launch.sh &
@@ -27,6 +27,8 @@ $HOME/.config/polybar/launch.sh &
 # else
 run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 # fi
+
+picom  --config $HOME/.config/bspwm/picom.conf &
 
 #Some ways to set your wallpaper besides variety or nitrogen
 # feh --bg-scale /home/arpangreat/dotfiles/wallpaper/devyg41-28cd223e-1a1f-48b6-87c7-41c0064fe163.jpg &
@@ -57,7 +59,6 @@ run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
 blueberry-tray &
-picom -b --animations --animation-window-mass 0.5 --animation-for-open-window zoom --animation-stiffness 350 --config $HOME/.config/bspwm/picom.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
 run volumeicon &
