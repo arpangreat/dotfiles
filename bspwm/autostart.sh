6fc888e7c9dfd80d8b795e0e403e412a9d8f47dc
arpangreat <arpanthegreat41@gmail.com>
2022-09-23 04:50:32 +0530

    polybar for bspwm

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -12,7 +12,7 @@ function run {
 #xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
 #xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
 #xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-xrandr --output HDMI-1 --mode 1920x1080 --rate 74.97 --primary --left-of eDP-1 --mode 1920x1080 --rate 60
+# xrandr --output HDMI-1 --mode 1920x1080 --rate 74.97 --primary --left-of eDP-1 --mode 1920x1080 --rate 60
 #autorandr horizontal
 
 $HOME/.config/polybar/launch.sh &
@@ -51,7 +51,7 @@ run kitty &
 # run alacritty &
 # run thunar & 
 # run dolphin &
-run emacs &
+# run emacs &
 run conky &
 run brave &
 run nm-applet &
