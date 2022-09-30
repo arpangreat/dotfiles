168d93e47bd683c4307558a25b8d720a3a1af9fe
arpangreat <arpanthegreat41@gmail.com>
2022-10-01 01:23:59 +0530

    catppuccin-ing for nvim and zsh

--- nvim/lua/nv-autocmds/init.lua
+++ nvim/lua/nv-autocmds/init.lua
@@ -4,3 +4,12 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		vim.lsp.codelens.refresh()
 	end,
 })
+
+-- PackerCompile on save if your config file is in plugins.lua or catppuccin.lua
+-- DO NOT put the autocmd inside the plugin specification file or you will get 2 ^ x files open after x saves
+--[[ vim.api.nvim_create_autocmd("BufWritePost", {
+	pattern = { "plugins.lua", "catppuccin.lua" },
+	callback = function()
+		vim.cmd("PackerCompile")
+	end,
+}) ]]
