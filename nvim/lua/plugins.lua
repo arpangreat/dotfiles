d87340359839b400ee2d5884b1286d1601dcd411
arpangreat <arpanthegreat41@gmail.com>
2022-10-10 19:21:24 +0530

    clean ups

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -83,6 +83,20 @@ return require("packer").startup({
 			},
 		})
 
+		-- Packer
+		use({
+			"folke/noice.nvim",
+			event = "VimEnter",
+			config = function()
+				require("noice").setup()
+			end,
+			requires = {
+				-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+				"MunifTanjim/nui.nvim",
+				"rcarriga/nvim-notify",
+			},
+		})
+
 		--[[ use({
 			"narutoxy/dim.lua",
 			config = function()
