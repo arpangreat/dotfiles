8e9b68dfadf1ec5e219f53e9e6de45e2778903d1
arpangreat <arpanthegreat41@gmail.com>
2022-10-13 14:31:28 +0530

    clean ups

--- nvim/lua/nv-dashboard/init.lua
+++ nvim/lua/nv-dashboard/init.lua
@@ -16,10 +16,16 @@ db.custom_header = header
 vim.g.indentLine_fileTypeExclude = { "dashboard" }
 
 db.custom_center = {
-	{ icon = " ", desc = "Find File              ", shortcut = "SPC t f", action = "Telescope find_files" },
-	{ icon = " ", desc = "Recently Used Files    ", shortcut = "SPC f h", action = "Telescope oldfiles" },
-	{ icon = " ", desc = "Find Word              ", shortcut = "SPC f a", action = "Telescope live_grep" },
-	{ icon = " ", desc = "Marks                  ", shortcut = "SPC f b", action = "Telescope marks" },
+	{ icon = "  ", desc = "Find File                ", shortcut = "SPC t f", action = "Telescope find_files" },
+	{ icon = "  ", desc = "Recently Used Files      ", shortcut = "SPC f h", action = "Telescope oldfiles" },
+	{ icon = "  ", desc = "Find Word                ", shortcut = "SPC f a", action = "Telescope live_grep" },
+	{ icon = "  ", desc = "Marks                    ", shortcut = "SPC f b", action = "Telescope marks" },
+	{
+		icon = "  ",
+		desc = "dotfiles               ",
+		action = "lua require('arpangreat.telescope').search_dotfiles()",
+		shortcut = "SPC t r c",
+	},
 }
 
 db.custom_footer = { "https://github.com/arpangreat/" }
