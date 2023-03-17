74b0f1e997996b71918dea1c568580f8d77a9be7
arpangreat <arpanthegreat41@gmail.com>
2023-03-17 21:10:43 +0530

    clean ups

--- nvim/lua/nv-indent-blankline/init.lua
+++ nvim/lua/nv-indent-blankline/init.lua
@@ -3,8 +3,14 @@ vim.opt.listchars:append("space:⋅")
 -- vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup({
+	char = "│", -- ┆ ┊ 
+	show_foldtext = false,
+	context_char = "▎",
+	char_priority = 12,
 	space_char_blankline = " ",
 	show_current_context = true,
 	show_current_context_start = true,
+	show_current_context_start_on_current_line = false,
 	use_treesitter = true,
+	show_first_indent_level = true,
 })
