22ad88aa41308ab738b3ef102ee9141c8aefee9c
Swastik Acharyya <arpanthegreat41@gmail.com>
2024-01-11 01:06:25 +0530

    clean ups

--- nvim/lua/plugins/sg.lua
+++ nvim/lua/plugins/sg.lua
@@ -1,6 +1,13 @@
 return {
-  "sourcegraph/sg.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
+	"sourcegraph/sg.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	event = "VeryLazy",
 
-  -- build = "nvim -l build/init.lua"
+	-- build = "nvim -l build/init.lua"
+	config = function()
+		local on_attach = require("plugins.lsp.config").on_attach
+		require("sg").setup({
+			on_attach = on_attach,
+		})
+	end,
 }
