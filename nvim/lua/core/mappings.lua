bfcd50624873580e7d97462b06e8786bb8fc6fec
Swastik Acharyya <arpanthegreat41@gmail.com>
2024-01-10 16:09:54 +0530

    clean ups

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -6,10 +6,10 @@
 
 -- HACK: YOU HAVE A MAPPING FOR Keymaps named nvmap
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>pv",
-  ":wincmd v<bar> :NvimTreeOpen <bar> :vertical resize 20<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>pv",
+	":wincmd v<bar> :NvimTreeOpen <bar> :vertical resize 20<CR>",
+	{ noremap = true, silent = false }
 )
 
 vim.api.nvim_set_keymap("n", "<Leader>u", ":MundoToggle<CR>", { noremap = true, silent = false })
@@ -31,162 +31,165 @@ vim.api.nvim_set_keymap("n", "\\gt", ":!go test<CR>", { noremap = true, silent =
 vim.api.nvim_set_keymap("n", "<Leader>fp", ":cprevious<CR>", { noremap = true, silent = false, expr = false })
 vim.api.nvim_set_keymap("n", "<Leader>fn", ":cnext<CR>", { noremap = true, silent = false, expr = false })
 
+-- FZFLua
+vim.api.nvim_set_keymap("n", "<Leader>fz", ":FzfLua<CR>", { noremap = true, silent = false, expr = false })
+
 -- Telescope
 
 vim.api.nvim_set_keymap("n", "<F8>", ":TagBarToggle<CR>", { noremap = false, silent = false })
 
 -- lua require("arpangreat")
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgf",
-  ":lua require('telescope.builtin').git_files()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgf",
+	":lua require('telescope.builtin').git_files()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgc",
-  ":lua require('telescope.builtin').git_commits()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgc",
+	":lua require('telescope.builtin').git_commits()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgb",
-  ":lua require('telescope.builtin').git_bcommits()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgb",
+	":lua require('telescope.builtin').git_bcommits()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgs",
-  ":lua require('telescope.builtin').git_status()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgs",
+	":lua require('telescope.builtin').git_status()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tf",
-  ":lua require('telescope').extensions.file_browser.file_browser({ hidden = true })<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tf",
+	":lua require('telescope').extensions.file_browser.file_browser({ hidden = true })<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tw",
-  ":lua require('telescope.builtin').grep_string({ prompt_prefix=🔍 })<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tw",
+	":lua require('telescope.builtin').grep_string({ prompt_prefix=🔍 })<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tb",
-  ":lua require('telescope.builtin').buffers()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tb",
+	":lua require('telescope.builtin').buffers()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<C-h>",
-  ":lua require('telescope.builtin').help_tags()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<C-h>",
+	":lua require('telescope.builtin').help_tags()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tm",
-  ":lua require('telescope.builtin').man_pages()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tm",
+	":lua require('telescope.builtin').man_pages()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tj",
-  ":lua require('telescope.builtin').jumplist()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tj",
+	":lua require('telescope.builtin').jumplist()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>trc",
-  ":lua require('arpangreat.telescope').search_dotfiles()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>trc",
+	":lua require('arpangreat.telescope').search_dotfiles()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>trg",
-  ":lua require('telescope.builtin').live_grep({ prompt_prefix=🔍 })<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>trg",
+	":lua require('telescope.builtin').live_grep({ prompt_prefix=🔍 })<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tcc",
-  ":lua require('arpangreat.telescope').search_configs()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tcc",
+	":lua require('arpangreat.telescope').search_configs()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tcb",
-  ":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tcb",
+	":lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgi",
-  ":lua require('telescope').extensions.gh.issues()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgi",
+	":lua require('telescope').extensions.gh.issues()<CR>",
+	{ noremap = true, silent = false }
 )
 
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgp",
-  ":lua require('telescope').extensions.gh.pull_request()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgp",
+	":lua require('telescope').extensions.gh.pull_request()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgg",
-  ":lua require('telescope').extensions.gh.gist()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tgg",
+	":lua require('telescope').extensions.gh.gist()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap("n", "<Leader>to", ":Telescope", { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "<Leader>ttc", ":TodoTelescope<CR>", { noremap = true, silent = false, expr = false })
 vim.keymap.set("n", "<Leader>ttt", ":Telescope telescope-tabs list_tabs<CR>")
 
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tea",
-  ":lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_cursor({}))<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>tea",
+	":lua require('telescope.builtin').lsp_code_actions(require('telescope.themes').get_cursor({}))<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>teo",
-  ":lua require('telescope.builtin').lsp_range_code_actions()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>teo",
+	":lua require('telescope.builtin').lsp_range_code_actions()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>ter",
-  ":lua require('telescope.builtin').lsp_references()<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>ter",
+	":lua require('telescope.builtin').lsp_references()<CR>",
+	{ noremap = true, silent = false }
 )
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tes",
-  ":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",
-  { noremap = true, silent = false }
-)
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>trf",
-  "<Cmd>lua require('telescope').extensions.frecency.frecency(require('telescope.themes').get_dropdown({ layout_config = { width = 0.25 } }))<CR>",
-  { noremap = true, silent = true }
+	"n",
+	"<Leader>tes",
+	":lua require('telescope.builtin').lsp_dynamic_workspace_symbols()<CR>",
+	{ noremap = true, silent = false }
 )
 
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tz",
-  "<Cmd>lua require'telescope'.extensions.zoxide.list{}<CR>",
-  { noremap = true, silent = false, expr = false }
+	"n",
+	"<Leader>trf",
+	"<Cmd>lua require('telescope').extensions.frecency.frecency(require('telescope.themes').get_dropdown({ layout_config = { width = 0.25 } }))<CR>",
+	{ noremap = true, silent = true }
 )
 
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>tgr",
-  "<Cmd>lua require('arpangreat.rg')<CR>",
-  { noremap = true, silent = false, expr = false }
+	"n",
+	"<Leader>tz",
+	"<Cmd>lua require'telescope'.extensions.zoxide.list{}<CR>",
+	{ noremap = true, silent = false, expr = false }
+)
+
+vim.api.nvim_set_keymap(
+	"n",
+	"<Leader>tgr",
+	"<Cmd>lua require('arpangreat.rg')<CR>",
+	{ noremap = true, silent = false, expr = false }
 )
 
 vim.api.nvim_set_keymap("n", "<Leader>gv", ":GitFiles<CR>", { noremap = true, silent = false })
@@ -205,10 +208,10 @@ vim.api.nvim_set_keymap("n", "<Leader>y", '"+y<CR>', { noremap = true, silent = 
 vim.api.nvim_set_keymap("n", "<Leader>ne", ":vsplit ~/wiki/index.md<CR>", { noremap = true, silent = false })
 vim.api.nvim_set_keymap("n", "<Leader>no", ":e ~/wiki/index.md<CR>", { noremap = true, silent = false })
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>ns",
-  ":lua require('arpangreat.telescope').search_notes(require('telescope.themes').get_dropdown({ layout_strategy = 'vertical' }))<CR>",
-  { noremap = true, silent = false }
+	"n",
+	"<Leader>ns",
+	":lua require('arpangreat.telescope').search_notes(require('telescope.themes').get_dropdown({ layout_strategy = 'vertical' }))<CR>",
+	{ noremap = true, silent = false }
 )
 
 -- Nvim Lsp mappings
@@ -264,27 +267,27 @@ local opts = { noremap = true, silent = true }
 
 -- HACK: Formating Big Lines with g
 vim.api.nvim_set_keymap(
-  "n",
-  "<Leader>fg",
-  "<cmd>g/ ./ normal gqq<CR>",
-  { noremap = true, silent = false, expr = false }
+	"n",
+	"<Leader>fg",
+	"<cmd>g/ ./ normal gqq<CR>",
+	{ noremap = true, silent = false, expr = false }
 )
 
 vim.keymap.set("n", "<Leader>rg", "<cmd>lua vim.lsp.codelens.run()<CR>")
 
 local ls = require("luasnip")
 vim.keymap.set("i", "<C-l>", function()
-  if ls.choice_active() then
-    ls.change_choice(1)
-  end
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
 end)
 
 -- Copilot
 vim.api.nvim_set_keymap(
-  "n",
-  "<c-s>",
-  "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>",
-  { noremap = true, silent = true }
+	"n",
+	"<c-s>",
+	"<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>",
+	{ noremap = true, silent = true }
 )
 
 -- Telekasten
