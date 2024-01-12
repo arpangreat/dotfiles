afd77346340c8c9722004d45ccbd9dd7ea8c0854
arpangreat <arpanthegreat41@gmail.com>
2024-01-12 22:49:25 +0530

    clean ups
    removing fzf-tab and neotab

---
+++ nvim/lua/plugins/neotab.lua
@@ -0,0 +1,14 @@
+return {
+	"kawre/neotab.nvim",
+	event = "InsertEnter",
+	opts = {
+		-- configuration goes here
+		smart_punctuators = {
+			enabled = true,
+			semicolon = {
+				enabled = true,
+				ft = { "cs", "c", "cpp", "java" },
+			},
+		},
+	},
+}
