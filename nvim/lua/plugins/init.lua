d7c293e7532cbce1fbddf7cb48162b9e6028e537
arpangreat <arpanthegreat41@gmail.com>
2024-02-19 12:50:20 +0530

    clean ups

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,55 +1,35 @@
 return {
-  "nvim-tree/nvim-web-devicons",
-  "sindrets/diffview.nvim",
-  "rhysd/committia.vim",
-  {
-    "uga-rosa/ccc.nvim",
-    init = function()
-      require("ccc").setup({
-        highlighter = {
-          auto_enable = true,
-          lsp = true,
-        },
-      })
-    end,
-  },
-  "dart-lang/dart-vim-plugin",
-  {
-    "akinsho/flutter-tools.nvim",
-    init = function()
-      require("flutter-tools").setup({
-        experimental = {
-          lsp_derive_paths = true,
-        },
-        widget_guides = {
-          enabled = true,
-        },
-      })
-    end,
-  },
-  "ThePrimeagen/harpoon",
-  { "simnalamburt/vim-mundo",                    event = "VeryLazy" },
-  {
-    "iamcco/markdown-preview.nvim",
-    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-  {
-    "echasnovski/mini.files",
-    event = "VeryLazy",
-    config = function()
-      require("mini.files").setup()
-    end,
-  },
-  {
-    "echasnovski/mini.cursorword",
-    event = "VeryLazy",
-    config = function()
-      require("mini.cursorword").setup()
-    end,
-  },
-  { "WhoIsSethDaniel/lualine-lsp-progress.nvim", event = "VeryLazy" },
+	"nvim-tree/nvim-web-devicons",
+	{
+		"uga-rosa/ccc.nvim",
+		init = function()
+			require("ccc").setup({
+				highlighter = {
+					auto_enable = true,
+					lsp = true,
+				},
+			})
+		end,
+	},
+	{ "simnalamburt/vim-mundo", event = "VeryLazy" },
+	{
+		"echasnovski/mini.files",
+		event = "VeryLazy",
+		config = function()
+			require("mini.files").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.cursorword",
+		event = "VeryLazy",
+		config = function()
+			require("mini.cursorword").setup()
+		end,
+	},
+	{ "WhoIsSethDaniel/lualine-lsp-progress.nvim", event = "VeryLazy" },
+	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		config = function() end,
+	},
 }
--- nvim/lua/plugins/lsp/config.lua
+++ nvim/lua/plugins/lsp/config.lua
@@ -1,133 +1,14 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-local protocol = require("vim.lsp.protocol")
-
-local lsp_highlight_document = function(client)
-	-- Set Autocommands conditional on server capabilities
-	if client.server_capabilities.document_highlight then
-		vim.api.nvim_exec(
-			[[
-      augroup lsp_document_highlight
-        autocmd! * <buffer>
-        autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-        autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-      augroup END
-    ]],
-			true
-		)
-	end
-end
-
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			return client.name == "null-ls"
-		end,
-		bufnr = bufnr,
-	})
-end
-
 local M = {}
 M.on_attach = function(client, bufnr)
-	local function buf_set_keymap(...)
-		vim.api.nvim_buf_set_keymap(bufnr, ...)
-	end
-	local function buf_set_option(...)
-		vim.api.nvim_buf_set_option(bufnr, ...)
-	end
+  if client.server_capabilities.documentSymbolProvider then
+    require("nvim-navic").attach(client, bufnr)
+  end
 
-	--Enable completion triggered by <c-x><c-o>
-	buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-	-- formatting
-	if client.server_capabilities.document_formatting then
-		vim.api.nvim_exec(
-			[[augroup Format
-			autocmd! * <buffer>
-			autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()
-			augroup END]],
-			true
-		)
-	end
-
-	--protocol.SymbolKind = { }
-	protocol.CompletionItemKind = {
-		"", -- Text
-		"", -- Method
-		"", -- Function
-		"", -- Constructor
-		"", -- Field
-		"", -- Variable
-		"", -- Class
-		"ﰮ", -- Interface
-		"", -- Module
-		"", -- Property
-		"", -- Unit
-		"", -- Value
-		"", -- Enum
-		"", -- Keyword
-		"﬌", -- Snippet
-		"", -- Color
-		"", -- File
-		"", -- Reference
-		"", -- Folder
-		"", -- EnumMember
-		"", -- Constant
-		"", -- Struct
-		"", -- Event
-		"ﬦ", -- Operator
-		"", -- TypeParameter
-	}
-
-	if client.supports_method("textDocument/formatting") then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-
-	if client.server_capabilities.documentSymbolProvider then
-		require("nvim-navic").attach(client, bufnr)
-	end
-
-	if client.server_capabilities.inlayHintProvider then
-		vim.lsp.inlay_hint.enable(bufnr, true)
-	end
+  if client.server_capabilities.inlayHintProvider then
+    vim.lsp.inlay_hint.enable(bufnr, true)
+  end
 end
 
 M.capabilities = require("cmp_nvim_lsp").default_capabilities()
-M.capabilities.textDocument.codeAction = {
-	dynamicRegistration = true,
-	codeActionLiteralSupport = {
-		codeActionKind = {
-			valueSet = (function()
-				local res = vim.tbl_values(vim.lsp.protocol.CodeActionKind)
-				table.sort(res)
-				return res
-			end)(),
-		},
-	},
-}
-
-M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities.textDocument.completion.completionItem.preselectSupport = true
-M.capabilities.textDocument.completion.completionItem.insertReplaceSupport = true
-M.capabilities.textDocument.completion.completionItem.labelDetailsSupport = true
-M.capabilities.textDocument.completion.completionItem.deprecatedSupport = true
-M.capabilities.textDocument.completion.completionItem.commitCharactersSupport = true
-M.capabilities.textDocument.completion.completionItem.tagSupport = { valueSet = { 1 } }
-M.capabilities.textDocument.completion.completionItem.resolveSupport = {
-	properties = {
-		"documentation",
-		"detail",
-		"additionalTextEdits",
-	},
-}
-
-M.capabilities.textDocument.colorProvider = { dynamciRegistration = true }
-M.capabilities.offsetEncoding = { "utf-16" }
 
 return M
