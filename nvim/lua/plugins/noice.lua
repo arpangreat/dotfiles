35c8c5cd6e510aed0a5f1dfb7a95f1d46e726e7d
arpangreat <arpanthegreat41@gmail.com>
2024-02-21 21:33:15 +0530

    clean ups

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,36 +1,29 @@
 return {
-  	"folke/noice.nvim",
-  	event = "VeryLazy",
-  	dependencies = {
-  		"MunifTanjim/nui.nvim",
-  	},
-    opts = {
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
       },
-      -- you can enable a preset for easier configuration
-      signature = {
-        enabled = false,
-        auto_open = {
-          enabled = false,
-          trigger = false, -- Automatically show signature help when typing a trigger character from the LSP
-          luasnip = false, -- Will open signature help when jumping to Luasnip insert nodes
-          throttle = 50, -- Debounce lsp signature help request by 50ms
-        },
-        view = nil, -- when nil, use defaults from documentation
-        opts = {}, -- merged with defaults from documentation
-      },
-      presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
-        command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = true, -- add a border to hover docs and signature help
-      },
-    }
+    },
+    popupmenu = {
+      backend = "cmp",
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true,      -- use a classic bottom cmdline for search
+      command_palette = true,    -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = true,         -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = true,     -- add a border to hover docs and signature help
+    },
+  },
 }
