fdccf125855fbb89fe9704f2bf4690d30d971cb2
arpangreat <arpanthegreat41@gmail.com>
2024-02-24 22:01:56 +0530

    clean ups

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -17,13 +17,43 @@ return {
     popupmenu = {
       backend = "cmp",
     },
+    views = {
+      cmdline_popup = {
+        position = {
+          row = 5,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = "auto",
+        },
+      },
+      popupmenu = {
+        relative = "editor",
+        position = {
+          row = 8,
+          col = "50%",
+        },
+        size = {
+          width = 60,
+          height = 10,
+        },
+        border = {
+          style = "rounded",
+          padding = { 0, 1 },
+        },
+        win_options = {
+          winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+        },
+      },
+    },
     -- you can enable a preset for easier configuration
     presets = {
-      bottom_search = true,      -- use a classic bottom cmdline for search
-      command_palette = true,    -- position the cmdline and popupmenu together
+      bottom_search = true,         -- use a classic bottom cmdline for search
+      command_palette = true,       -- position the cmdline and popupmenu together
       long_message_to_split = true, -- long messages will be sent to a split
-      inc_rename = true,         -- enables an input dialog for inc-rename.nvim
-      lsp_doc_border = true,     -- add a border to hover docs and signature help
+      inc_rename = true,            -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = true,        -- add a border to hover docs and signature help
     },
   },
 }
