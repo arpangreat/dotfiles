0f73d06ebb7f0b2286804c6d35c440f13ae381f1
arpangreat <arpanthegreat41@gmail.com>
2024-02-26 22:48:25 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/conf.d/abbr.fish
+++ fish/conf.d/abbr.fish
@@ -1,26 +1,10 @@
-abbr -a nv nvim
-abbr -a e emacs
-abbr -a en emacs -nw
-abbr -a brc nvim ~/dotfiles/bspwm/
-abbr -a hrc nvim ~/dotfiles/herbstluftwm/
-abbr -a vrc vim ~/.vimrc
-abbr -a ec emacsclient
-abbr -a nrc nvim ~/dotfiles/nvim/init.lua
-abbr -a g++ g++ -std c++2a
-abbr -a gf $GOPATH/bin/gf
-abbr -a arc nvim ~/dotfiles/alacritty/alacritty.yml
-abbr -a zrc nvim ~/.zshrc
-abbr -a trc nvim ~/.tmux.conf
-abbr -a krc nvim ~/dotfiles/kitty/kitty.conf
-abbr -a irc nvim ~/dotfiles/i3/config
-abbr -a sld sway-launcher-desktop
-abbr -a tm tmux -u
-abbr -a cht ~/dotfiles/custom-scripts/tmux-cht.sh
-abbr -a notes nvim ~/wiki/index.md
-abbr -a lg lazygit
-abbr -a .. cd ../
-abbr -a ... cd ../../
-abbr -a .... cd ../../../
-abbr -a ..... cd ../../../../
-abbr -a open xdg-open
-abbr -a man batman
+abbr nv nvim
+abbr e emacs
+abbr en emacs -nw
+abbr lg lazygit
+abbr .. cd ../
+abbr ... cd ../../
+abbr .... cd ../../../
+abbr ..... cd ../../../../
+abbr open xdg-open
+abbr man batman
