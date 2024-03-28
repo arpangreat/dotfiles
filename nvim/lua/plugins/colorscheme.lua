e632639d78e7aedd0f916059ae0b4ffde54d12bf
arpangreat <arpanthegreat41@gmail.com>
2024-03-28 07:52:09 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -60,7 +60,7 @@ return {
 			-- show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 			term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
 			dim_inactive = {
-				enabled = true, -- dims the background color of inactive window
+				enabled = false, -- dims the background color of inactive window
 				shade = "dark",
 				percentage = 0.15, -- percentage of the shade to apply to the inactive window
 			},
