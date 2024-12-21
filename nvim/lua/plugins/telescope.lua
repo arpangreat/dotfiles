2b3d00d233957c52e45b1c113d48865d2759e0e8
arpangreat <arpanthegreat41@gmail.com>
2024-12-21 20:56:01 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,7 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	event = "VeryLazy",
-	enabled = false,
+	-- enabled = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
