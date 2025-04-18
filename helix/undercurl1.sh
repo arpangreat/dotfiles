52302f68721de595486a7d500a7c416fc58c977a
arpangreat <arpanthegreat41@gmail.com>
2025-04-18 16:49:59 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- helix/undercurl1.sh
+++ helix/undercurl1.sh
@@ -1,2 +1,4 @@
 printf '\e[4:1mHello, underline!\n\e[m\e[4:2mHello, double-underline!\n\e[m\e[4:3mHello, under-curl!\n\e[m\e[4:4mHello, under-dotted!\n\e[m\e[4:5mHello, under-dashed!\n\e[m\e[58;2;255;0;0m\e[4:1mHello, underline!\n\e[m\e[58;2;255;0;0m\e[4:2mHello, double-underline!\n\e[m\e[58;2;255;0;0m\e[4:3mHello, under-curl!\n\e[m\e[58;2;255;0;0m\e[4:4mHello, under-dotted!\n\e[m\e[58;2;255;0;0m\e[4:5mHello, under-dashed!\n\e[m
+
+
 '
