94cd26c68f3841ff0f749dc97492ca9e413e7c67
arpangreat <arpanthegreat41@gmail.com>
2025-04-20 01:43:06 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/lsp/go.lua
+++ nvim/lua/plugins/lsp/go.lua
@@ -4,9 +4,9 @@ local capabilities = require("plugins.lsp.config").capabilities
 local lspconfig = require("lspconfig")
 
 lspconfig.gopls.setup({
-	cmd = { "gopls", "serve" },
-	filetypes = { "go", "gomod" },
-	root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+	cmd = { "gopls" },
+	-- filetypes = { "go", "gomod" },
+	-- root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
 	settings = {
 		gopls = {
 			codelenses = { generate = true },
@@ -27,30 +27,4 @@ lspconfig.gopls.setup({
 	},
 	on_attach = on_attach,
 	capabilities = capabilities,
-})
-
-local configs = require("lspconfig/configs")
-
-if not configs.golangcilsp then
-	configs.golangcilsp = {
-		default_config = {
-			cmd = { "golangci-lint-langserver" },
-			root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
-			init_options = {
-				command = {
-					"golangci-lint",
-					"run",
-					"--enable-all",
-					"--disable",
-					"lll",
-					"--out-format",
-					"json",
-					"--issues-exit-code=1",
-				},
-			},
-		},
-	}
-end
-lspconfig.golangci_lint_ls.setup({
-	filetypes = { "go", "gomod" },
 })
