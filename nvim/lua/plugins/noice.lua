5173231fda8aa7c8b21a10d30ffbec2c83b9b3cd
arpangreat <arpanthegreat41@gmail.com>
2025-04-28 22:32:48 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -19,7 +19,8 @@ return {
 			},
 		},
 		-- popupmenu = {
-		-- 	backend = "nui",
+		-- 	-- backend = "nui",
+		-- 	-- enabled = false,
 		-- },
 
 		views = {
@@ -70,7 +71,7 @@ return {
 			bottom_search = true, -- use a classic bottom cmdline for search
 			-- command_palette = true, -- position the cmdline and popupmenu together
 			-- long_message_to_split = true, -- long messages will be sent to a split
-			inc_rename = true, -- enables an input dialog for inc-rename.nvim
+			inc_rename = false, -- enables an input dialog for inc-rename.nvim
 			lsp_doc_border = true, -- add a border to hover docs and signature help
 		},
 	},
