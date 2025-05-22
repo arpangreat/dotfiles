fd97b7356edd09313585071e9a0ec30a594db536
arpangreat <arpanthegreat41@gmail.com>
2025-05-22 22:46:44 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/lsp/rust.lua
+++ nvim/lua/plugins/lsp/rust.lua
@@ -49,6 +49,8 @@ vim.lsp.config("rust_analyzer", {
 			-- on_attach = my_custom_attach,
 		},
 	},
+	on_attach = require("plugins.lsp.config").on_attach,
+	capabilities = require("plugins.lsp.config").capabilities,
 })
 
 vim.lsp.enable("rust_analyzer")
