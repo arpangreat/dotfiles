db262988308958ed80e3ffc86df4c0d7db08cb31
arpangreat <arpanthegreat41@gmail.com>
2025-07-19 19:17:31 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- hypr/launch.sh
+++ hypr/launch.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-sleep 4
-killall xdg-desktop-portal-wlr
+sleep 1
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
-/usr/lib/xdg-desktop-portal-wlr &
-sleep 4
+echo "Starting xdg-desktop-portal-hyprland" >> xdg-desktop-portal-hyprland.log
+/usr/lib/xdg-desktop-portal-hyprland >> xdg-desktop-portal-hyprland.log 2>&1 &
+sleep 2
 /usr/lib/xdg-desktop-portal &
