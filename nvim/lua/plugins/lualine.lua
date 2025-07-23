4a068ca72a063b32a9aa5d9ef95c4d857432cd2b
arpangreat <arpanthegreat41@gmail.com>
2025-07-23 21:18:29 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -74,7 +74,7 @@ return {
 							hint = "󰝶 ",
 						},
 					},
-					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ "filetype", icon_only = true, separator = "", padding = { left = 0, right = 0 } },
 					{ "filename", padding = { left = 1, right = 0 } },
 					{
 						function()
@@ -104,9 +104,7 @@ return {
 					"filetype",
 				},
 				lualine_z = {
-					{
-						"progress",
-					},
+					"progress",
 					{
 						"location",
 						color = utils.get_hlgroup("Boolean"),
@@ -127,97 +125,4 @@ return {
 			extensions = { "lazy", "mason", "trouble" },
 		}
 	end,
-	-- config = function()
-	-- 	local icons = require("user.icons")
-	-- 	local diff = {
-	-- 		"diff",
-	-- 		colored = true,
-	-- 		symbols = {
-	-- 			added = icons.git.LineAdded,
-	-- 			modified = icons.git.LineModified,
-	-- 			removed = icons.git.LineRemoved,
-	-- 		}, -- Changes the symbols used by the diff.
-	-- 	}
-
-	-- 	local clients_lsp = function()
-	-- 		local bufnr = vim.api.nvim_get_current_buf()
-
-	-- 		local clients = vim.lsp.get_clients({ bufnr = bufnr })
-	-- 		if next(clients) == nil then
-	-- 			return ""
-	-- 		end
-
-	-- 		local c = {}
-	-- 		for _, client in pairs(clients) do
-	-- 			table.insert(c, client.name)
-	-- 		end
-	-- 		return "\u{f085} " .. table.concat(c, "|")
-	-- 	end
-
-	-- 	local custom = require("lualine.themes.catppuccin")
-	-- 	-- local custom = require("lualine.themes.tokyonight")
-	-- 	-- local custom = require("lualine.themes.material")
-	-- 	-- local custom = require("lualine.themes.horizon")
-	-- 	-- local custom = require("lualine.themes.solarized-osaka")
-	-- 	-- local custom = require("lualine.themes.eldritch")
-	-- 	custom.normal.c.bg = "none"
-
-	-- 	local config = {
-	-- 		options = {
-	-- 			-- theme = "catppuccin",
-	-- 			-- theme = "onedark_vivid",
-	-- 			theme = custom,
-	-- 			-- theme = "bamboo",
-	-- 			-- theme = "solarized-osaka",
-	-- 			-- theme = "tokyonight",
-	-- 			-- theme = "carbonfox",
-	-- 			-- theme = "material",
-	-- 			-- theme = "themer",
-	-- 			component_separators = { left = "", right = "" },
-	-- 			section_separators = { left = "", right = "" },
-
-	-- 			fmt = string.lower,
-	-- 			-- refresh = {
-	-- 			-- 	statusline = nil,
-	-- 			-- },
-	-- 		},
-	-- 		sections = {
-	-- 			lualine_a = {
-	-- 				{
-	-- 					"mode",
-	-- 					fmt = function(str)
-	-- 						return str:sub(1, 1)
-	-- 					end,
-	-- 				},
-	-- 			},
-	-- 			lualine_b = { "branch" },
-	-- 			lualine_c = {
-	-- 				diff,
-	-- 				--[[ {
-	-- 					function()
-	-- 						return require("nvim-navic").get_location()
-	-- 					end,
-	-- 					cond = function()
-	-- 						return require("nvim-navic").is_available()
-	-- 					end,
-	-- 				}, ]]
-	-- 			},
-	-- 			lualine_y = { "filetype" },
-	-- 			lualine_z = { "progress", "location" },
-	-- 			lualine_x = {
-	-- 				"encoding",
-	-- 				clients_lsp,
-	-- 				"fileformat",
-	-- 			},
-	-- 		},
-	-- 		tabline = {
-	-- 			lualine_z = { {
-	-- 				"tabs",
-	-- 				mode = 2,
-	-- 			} },
-	-- 		},
-	-- 	}
-
-	-- 	require("lualine").setup(config)
-	-- end,
 }
