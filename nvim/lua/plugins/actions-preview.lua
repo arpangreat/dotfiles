b3c24ec99994b7aa452233bc0cda2fbb8e1a9653
arpangreat <arpanthegreat41@gmail.com>
2025-07-25 14:24:48 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/actions-preview.lua
+++ nvim/lua/plugins/actions-preview.lua
@@ -1,6 +1,6 @@
 return {
 	"aznhe21/actions-preview.nvim",
-	-- enabled = false,
+	enabled = false,
 	event = "LspAttach",
 	config = function()
 		require("actions-preview").setup()
--- nvim/lua/plugins/lsp/config.lua
+++ nvim/lua/plugins/lsp/config.lua
@@ -6,25 +6,22 @@ M.on_attach = function(client, bufnr)
 	client.server_capabilities.documentRangeFormattingProvider = false
 	client.server_capabilities.didSave = false
 
-	vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "K", "<cmd>lua require('pretty_hover').hover()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gd", "<cmd>FzfLua lsp_definitions<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "K", "<cmd>FzfLua require('pretty_hover').hover()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gi", "<cmd>FzfLua lsp_implementations<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gs", "<cmd>FzfLua vim.lsp.buf.signature_help()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gD", "<cmd>FzfLua lsp_declarations<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "go", "<cmd>FzfLua lsp_typedefs<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gR", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "g0", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gW", "<cmd>lua vim.lsp.buf.workspace_symbol()<CR>", { noremap = true, silent = true })
-	vim.keymap.set(
-		"n",
-		"ga",
-		"<cmd>lua require('actions-preview').code_actions()<CR>",
-		{ noremap = true, silent = true }
-	)
+	vim.keymap.set("n", "gR", "<cmd>FzfLua lsp_references<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "g0", "<cmd>FzfLua lsp_document_symbols<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gW", "<cmd>FzfLua lsp_workspace_symbols<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "ga", "<cmd>FzfLua lsp_code_actions<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "gk", "<cmd>lua vim.diagnostic.jump({count = -1})<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "gj", "<cmd>lua vim.diagnostic.jump({count = 1})<CR>", { noremap = true, silent = true })
-	vim.keymap.set("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gl", "<cmd>FzfLua lsp_document_diagnostics<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gL", "<cmd>FzfLua lsp_workspace_diagnostics<CR>", { noremap = true, silent = true })
+	vim.keymap.set("n", "gF", "<cmd>FzfLua lsp_finder<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "gq", "<cmd>lua vim.diagnostic.setqflist()<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "gt", "<cmd>lua vim.diagnostic.setloclist()<CR>", { noremap = true, silent = true })
 	vim.keymap.set("n", "<Leader>rg", "<cmd>lua vim.lsp.codelens.run()<CR>")
