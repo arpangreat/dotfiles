85201b240d139881942e5a0d182be69511e07b88
arpangreat <arpanthegreat41@gmail.com>
2025-08-01 21:40:17 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

---
+++ nvim/lua/plugins/markdown.lua
@@ -0,0 +1,21 @@
+-- For `plugins/markview.lua` users.
+return {
+	"OXY2DEV/markview.nvim",
+	lazy = true,
+	ft = "markdown",
+
+	-- For `nvim-treesitter` users.
+	priority = 49,
+
+	-- For blink.cmp's completion
+	-- source
+	-- dependencies = {
+	--     "saghen/blink.cmp"
+	-- },
+	opts = {
+		experimental = {
+			check_rtp = false,
+			check_rtp_message = false,
+		},
+	},
+}
