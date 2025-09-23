b42b882623c1a3575b12843db08709b104d3e83b
arpangreat <arpanthegreat41@gmail.com>
2025-09-23 20:43:59 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -35,7 +35,7 @@ vim.opt.winblend = 0
 vim.opt.pumblend = 0
 vim.opt.textwidth = 80
 vim.opt.ruler = true
--- vim.opt.winborder = "single"
+vim.opt.winborder = "single"
 -- vim.opt.cmdheight = 0
 
 -- Visuals
