3796544fe861650f761274b0e28adf39ba3f5d2d
arpangreat <arpanthegreat41@gmail.com>
2025-10-25 13:30:16 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/lta.fish
+++ fish/functions/lta.fish
@@ -1,3 +1,3 @@
 function lta
-    command eza -l -g --git --tree -a --color=always --header --hyperlink | bat
+    command eza -l -g --git --tree -a --color=always --header --hyperlink --icons=auto | bat
 end
