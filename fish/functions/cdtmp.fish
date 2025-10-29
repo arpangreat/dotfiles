c29aefc23f2986d4ac2313b4009e978bf9ad9ede
arpangreat <arpanthegreat41@gmail.com>
2025-10-29 22:06:24 +0530

    clean up s
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/cdtmp.fish
+++ fish/functions/cdtmp.fish
@@ -1,5 +1,14 @@
 function cdtmp
     set tmpdir (mktemp -d /tmp/tmpdir.XXXXXXXX)
     echo "Created: $tmpdir"
+
+    function _cdtmp_cleanup --on-variable PWD --inherit-variable tmpdir
+        if test "$PWD" != "$tmpdir"
+            echo "Leaving $tmpdir — deleting..."
+            rm -rf "$tmpdir"
+            functions -e _cdtmp_cleanup
+        end
+    end
+
     cd $tmpdir
 end
