9142bb039c934a7b26620d88e4a4c9d9f1c9c98f
arpangreat <arpanthegreat41@gmail.com>
2025-10-31 21:23:04 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -13,11 +13,6 @@ return {
 		transparent_background = true, -- disables setting the background color.
 		-- show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
 		term_colors = true, -- sets terminal colors (e.g. `g:terminal_color_0`)
-		dim_inactive = {
-			enabled = false, -- dims the background color of inactive window
-			shade = "dark",
-			percentage = 0.15, -- percentage of the shade to apply to the inactive window
-		},
 		-- no_italic = false, -- Force no italic
 		-- no_bold = false, -- Force no bold
 		-- no_underline = false, -- Force no underline
