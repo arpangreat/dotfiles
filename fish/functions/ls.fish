81ce53a0bfcf58a8ef511e4183a2e4d5b76f30af
arpangreat <arpanthegreat41@gmail.com>
2025-10-31 12:42:47 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/ls.fish
+++ fish/functions/ls.fish
@@ -1,3 +1,3 @@
 function ls
-    command eza -l -g --git --color=always --header --hyperlink --icons=auto $argv[1] | less
+    command eza -l -g --git --color=always --header --hyperlink --icons=auto $argv[1] | bat
 end
