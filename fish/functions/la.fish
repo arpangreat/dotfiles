7adf90610897d8667506c727226bd3f9ea58048c
arpangreat <arpanthegreat41@gmail.com>
2025-10-31 12:42:47 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/la.fish
+++ fish/functions/la.fish
@@ -1,3 +1,3 @@
 function la
-    command eza -l -g --git -a --color=always --header --hyperlink --icons=auto $argv[1] | less
+    command eza -l -g --git -a --color=always --header --hyperlink --icons=auto $argv[1] | bat
 end
--- fish/functions/lta.fish
+++ fish/functions/lta.fish
@@ -1,3 +1,3 @@
 function lta
-    command eza -l -g --git --tree -a --color=always --header --hyperlink --icons=auto | bat
+    command eza -l -g --git --tree -a --color=always --header --hyperlink --icons=auto $argv[1] | bat
 end
