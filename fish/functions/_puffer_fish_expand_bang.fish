c82b099fe50872141467494213fdc90efec5a3c8
arpangreat <arpanthegreat41@gmail.com>
2025-11-16 18:02:28 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/_puffer_fish_expand_bang.fish
+++ fish/functions/_puffer_fish_expand_bang.fish
@@ -1,14 +1,10 @@
 function _puffer_fish_expand_bang
-    set -l current_token (commandline -t)
-    if not test (count $current_token) -eq 1
-        commandline -i '!'
-        return
-    end
-    switch (commandline -t)
-      case '!'
-        commandline -t $history[1]
-      case '*'
-        commandline -i '!'
+    if commandline --search-field >/dev/null
+        commandline --search-field --insert '!'
+    else if string match --quiet -- '!' "$(commandline --current-token)"
+        commandline --current-token $history[1]
+    else
+        commandline --insert '!'
     end
 end
 
