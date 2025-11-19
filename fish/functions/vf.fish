b5e4871e43736eb3f092ae8870ca0889b1c2fab4
arpangreat <arpanthegreat41@gmail.com>
2025-11-19 18:47:23 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/vf.fish
+++ fish/functions/vf.fish
@@ -1,5 +1,22 @@
 function vf
-    set -l files (locate -Ai -0 $argv | grep -z -vE '~$' | fzf-tmux --read0 -0 -1 -m)
+    if test (count $argv) -eq 0
+        echo "Usage: vf <regex>"
+        return 1
+    end
+
+    set -l query $argv
+
+    if test (count $matches) -eq 0
+        echo "No matches found for: $query"
+        return 1
+    end
+
+    set -l files (
+        rg --null --files-with-matches --hidden --no-ignore-vcs "$query" . \
+        | fzf --read0 -0 -1 -m \
+            --preview "rg --color=always --line-number --context=5 '$query' {} \
+            | bat --style=numbers --color=always --paging=never"
+    )
 
     if test -n "$files"
         $EDITOR -- $files
