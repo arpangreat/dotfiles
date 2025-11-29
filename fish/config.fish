cbeaafe7b0493dd123ee14da6ed4c31d90534f74
arpangreat <arpanthegreat41@gmail.com>
2025-11-30 00:04:41 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -126,7 +126,7 @@ set -gx ARGC_COMPLETIONS_ROOT /home/arpangreat/argc-completions
 set -gx ARGC_COMPLETIONS_PATH "$ARGC_COMPLETIONS_ROOT/completions/linux:$ARGC_COMPLETIONS_ROOT/completions"
 fish_add_path "$ARGC_COMPLETIONS_ROOT/bin"
 # To add completions for only the specified command, modify next line e.g. set argc_scripts cargo git
-set argc_scripts (ls -1 "$ARGC_COMPLETIONS_ROOT/completions/linux" "$ARGC_COMPLETIONS_ROOT/completions" | sed -n 's/\.sh$//p')
+set argc_scripts (command ls -1 "$ARGC_COMPLETIONS_ROOT/completions/linux" "$ARGC_COMPLETIONS_ROOT/completions" | string replace -r '\.sh$' '')
 argc --argc-completions fish $argc_scripts | source
 
 source ~/.safe-chain/scripts/init-fish.fish # Safe-chain Fish initialization script
