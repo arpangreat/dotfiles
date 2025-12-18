857a8448f48a0aacde56da10afd7331a22db226b
arpangreat <arpanthegreat41@gmail.com>
2025-12-18 22:01:56 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/fzflazy.lua
+++ nvim/lua/plugins/fzflazy.lua
@@ -4,14 +4,32 @@ return {
 	keys = {
 		{ "<Leader>fz", ":FzfLua ", desc = "FzfLua" },
 		{ "<Leader>ff", "<cmd>FzfLua files<CR>", desc = "Find files" },
-		{ "<Leader>fll", function() require("fzf-lua-lazy").search() end, desc = "Search lazy plugins" },
+		{
+			"<Leader>fll",
+			function()
+				require("fzf-lua-lazy").search()
+			end,
+			desc = "Search lazy plugins",
+		},
 		{ "<Leader>fw", "<cmd>FzfLua live_grep<CR>", desc = "Live grep" },
 		{ "<Leader>fb", "<cmd>FzfLua buffers<CR>", desc = "Buffers" },
 		{ "<Leader>fh", "<cmd>FzfLua helptags<CR>", desc = "Help tags" },
 		{ "<Leader>fm", "<cmd>FzfLua manpages<CR>", desc = "Man pages" },
 		{ "<Leader>fj", "<cmd>FzfLua jump<CR>", desc = "Jump list" },
-		{ "<Leader>frc", function() require("fzf-lua").files({ prompt = "Dotfiles> ", cwd = "~/dotfiles/nvim" }) end, desc = "Dotfiles" },
-		{ "<Leader>fcc", function() require("fzf-lua").files({ prompt = "Config> ", cwd = "~/dotfiles" }) end, desc = "Config files" },
+		{
+			"<Leader>frc",
+			function()
+				require("fzf-lua").files({ prompt = "Dotfiles> ", cwd = "~/dotfiles/nvim" })
+			end,
+			desc = "Dotfiles",
+		},
+		{
+			"<Leader>fcc",
+			function()
+				require("fzf-lua").files({ prompt = "Config> ", cwd = "~/dotfiles" })
+			end,
+			desc = "Config files",
+		},
 		{ "<Leader>fcb", "<cmd>FzfLua lgrep_curbuf<CR>", desc = "Grep current buffer" },
 	},
 	-- enabled = false,
@@ -40,11 +58,11 @@ return {
 				["ctrl-u"] = "preview-page-up",
 			},
 		},
-		lsp = {
-			code_actions = {
-				previewer = "codeaction_native",
-			},
-		},
+		-- lsp = {
+		-- 	code_actions = {
+		-- 		previewer = "codeaction_native",
+		-- 	},
+		-- },
 		-- config.defaults.keymap.fzf["alt-n"] = "preview-page-down"
 		-- config.defaults.keymap.fzf["alt-n"] = "preview-page-down"
 		-- config.defaults.keymap.builtin["<alt-p>"] = "preview-page-up"
