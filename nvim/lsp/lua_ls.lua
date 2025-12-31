012fdf9e8309c12e1717317d53a5b02ad7e0d0a0
arpangreat <arpanthegreat41@gmail.com>
2025-12-31 19:19:52 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lsp/lua_ls.lua
+++ nvim/lsp/lua_ls.lua
@@ -26,9 +26,8 @@ return {
 			workspace = {
 				checkThirdParty = false,
 				library = {
-					vim.env.VIMRUNTIME,
-					"${3rd}/luv/library",
 					vim.api.nvim_get_runtime_file("", true),
+					"${3rd}/luv/library",
 				},
 			},
 		},
