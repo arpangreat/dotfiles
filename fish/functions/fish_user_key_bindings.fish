d2e8aed3dd405fc87e9547ea65c17fe4ff2119a2
arpangreat <arpanthegreat41@gmail.com>
2026-02-09 23:45:57 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,3 +1,5 @@
 function fish_user_key_bindings
-    fzf --fish | source
+  fzf --fish | source
+    source /home/arpangreat/fzf/shell/completion.fish
+    source /home/arpangreat/fzf/shell/key-bindings.fish
 end
