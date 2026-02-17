7215918723dba53fda6c3c9e7708285f74ea84af
arpangreat <arpanthegreat41@gmail.com>
2026-02-18 00:09:49 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -106,6 +106,7 @@ zoxide init fish --cmd cd | source
 cod init $fish_pid fish | source
 
 source /home/arpangreat/ghostty/zig-out/share/fish/vendor_completions.d/ghostty.fish
+source /home/arpangreat/ghostty/zig-out/share/ghostty/shell-integration/fish/vendor_conf.d/ghostty-shell-integration.fish
 source /home/arpangreat/.local/share/nvim/lazy/tokyonight.nvim/extras/fish/tokyonight_moon.fish
 
 # opam configuration
