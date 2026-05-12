1e5ef2a8d60c2cab7a2ee6060795d92f551bc96b
arpangreat <arpanthegreat41@gmail.com>
2026-05-12 19:52:00 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -4,7 +4,7 @@ local main_mod = "SUPER"
 
 hl.monitor({ output = "HDMI-A-1", mode = "1920x1080@75", position = "0x0", scale = "1" })
 hl.monitor({ output = "eDP-1", mode = "1920x1080@60", position = "1920x0", scale = "1" })
-hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "auto" })
+hl.monitor({ output = "", mode = "preferred", position = "auto", scale = "1" })
 
 hl.on("hyprland.start", function()
 	for _, cmd in ipairs({
