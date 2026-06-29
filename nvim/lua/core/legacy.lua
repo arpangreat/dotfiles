8f96efc32079c4524f837c1fab06edf0b2b6d637
arpangreat <arpanthegreat41@gmail.com>
2026-06-29 20:09:01 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/core/legacy.lua
+++ nvim/lua/core/legacy.lua
@@ -1,3 +1,20 @@
+for _, plugin in ipairs({
+	"netrw",
+	"netrwPlugin",
+	"gzip",
+	"tarPlugin",
+	"tohtml",
+	"tutor",
+	"zipPlugin",
+	"syntax",
+	"Syntax",
+}) do
+	vim.g["loaded_" .. plugin] = 1
+end
+
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_2html_plugin = 1
+
 pcall(vim.cmd.packadd, "nvim.undotree")
 pcall(vim.cmd.packadd, "nvim.difftool")
 
