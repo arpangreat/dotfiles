e5d522af99c139d5118e82edd0c440742df5cbbb
arpangreat <arpanthegreat41@gmail.com>
2026-07-13 20:32:03 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/plugin/03-blink.lua
+++ nvim/plugin/03-blink.lua
@@ -7,7 +7,8 @@ require("vim-pack").add({
 		on_setup = function()
 			-- PackChanged fires during vim.pack.add() on install/update.
 			-- Complete the native matcher build before configuring blink.cmp.
-			require("core.blink_build").run_if_required()
+			-- require("core.blink_build").run_if_required()
+			require("blink.cmp").build():pwait()
 			require("plugins.blink").setup()
 		end,
 	},
