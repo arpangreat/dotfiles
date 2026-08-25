e1c0c6893f20cfcd67536f3f045a78c4959a39ca
arpangreat <arpanthegreat41@gmail.com>
2026-08-25 22:39:24 +0530

    clean ups
    Signed-off-by: arpangreat <arpanthegreat41@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local install_dir = vim.fn.stdpath("data") .. "/site"
+
 local parsers = {
 	"rust",
 	"zig",
@@ -26,13 +28,20 @@ local parsers = {
 	-- "ghostty",
 }
 
+local lang_by_filetype = {
+	javascriptreact = "javascript",
+	typescriptreact = "tsx",
+}
+
 local filetypes = {
 	"rust",
 	"zig",
 	"javascript",
+	"javascriptreact",
 	"cpp",
 	"c",
 	"typescript",
+	"typescriptreact",
 	"go",
 	"gomod",
 	"java",
@@ -51,17 +60,33 @@ local filetypes = {
 	-- "ghostty",
 }
 
-function M.setup()
-	require("nvim-treesitter").setup({
-		install_dir = vim.fn.stdpath("data") .. "/site",
-	})
+local function prepend_runtime_queries()
+	local paths = vim.api.nvim_get_runtime_file("lua/nvim-treesitter/init.lua", false)
+	local plugin_root = paths[1] and vim.fs.dirname(vim.fs.dirname(vim.fs.dirname(paths[1])))
+	local runtime_dir = plugin_root and vim.fs.joinpath(plugin_root, "runtime")
 
-	require("nvim-treesitter").install(parsers)
+	if runtime_dir and vim.uv.fs_stat(runtime_dir) then
+		vim.opt.runtimepath:prepend(runtime_dir)
+	end
+end
+
+function M.setup()
+	local treesitter = require("nvim-treesitter")
+
+	treesitter.setup({ install_dir = install_dir })
+	prepend_runtime_queries()
+
+	for filetype, lang in pairs(lang_by_filetype) do
+		vim.treesitter.language.register(lang, filetype)
+	end
+
+	treesitter.install(parsers)
 
 	vim.api.nvim_create_autocmd("FileType", {
 		pattern = filetypes,
 		callback = function()
 			vim.treesitter.start()
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end,
 	})
 end
